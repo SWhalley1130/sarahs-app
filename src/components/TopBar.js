c97a4ae5db6803849fb2199355ea434c1edbd6b8
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-07 15:08:25 -0700

    Change icon in left corner

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { NavLink } from "react-router-dom";
-import ship from "../images/ship.png";
-import tree from "../images/tree.png";
-import safety from "../images/safety.avif";
+import yea from "../images/yea.png";
 import Nav from 'react-bootstrap/Nav';
 
 function TopBar(props)
@@ -11,7 +9,7 @@ function TopBar(props)
     return (
        <Nav className="topbar">
             <Container style={{display:'flex', gap:'2% 2%', alignItems:'center'}}>
-                <NavLink to="/"><img className="ship-logo" src={safety}/></NavLink>
+                <NavLink to="/"><img className="ship-logo" src={yea}/></NavLink>
                 {props.children}
             </Container>
        </Nav>
