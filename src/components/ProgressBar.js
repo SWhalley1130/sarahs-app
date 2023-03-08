7038ac643de17e2b55a93e9771576a955ae51aa7
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-08 16:55:47 -0700

    Styling changes

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -3,42 +3,44 @@ import React from "react";
 function ProgressBar({trip})
 {
     let sum=trip.cruisePrice;
-            trip.flights.forEach(fl=>sum=sum+fl.price);
-            trip.hotels.forEach(ho=>sum=sum+ho.price);
-            trip.activities.forEach(act=>sum=sum+act.price);
+    trip.flights.forEach(fl=>sum=sum+fl.price);
+    trip.hotels.forEach(ho=>sum=sum+ho.price);
+    trip.activities.forEach(act=>sum=sum+act.price);
+
+    let percent=((sum/trip.budget)*100);
+
+    const containerStyles = {
+        height: 20,
+        width: '100%',
+        backgroundColor: "#e0e0de",
+        borderRadius: 50,
+        marginTop: 10,
+        marginBottom: 10
+      }
     
-            let percent=((sum/trip.budget)*100)
+      const fillerStyles = {
+        height: '100%',
+        width: `${percent>100 ? 100 :percent}%`,
+        backgroundColor: `${percent>100 ? 'red' : '#6ab89e'}`,
+        borderRadius: 'inherit',
+        textAlign: 'center'
+      }
     
-            const containerStyles = {
-                height: 20,
-                width: '100%',
-                backgroundColor: "#e0e0de",
-                borderRadius: 50,
-                marginTop: 10,
-                marginBottom: 10
-              }
-            
-              const fillerStyles = {
-                height: '100%',
-                width: `${percent}%`,
-                backgroundColor: '#6ab89e',
-                borderRadius: 'inherit',
-                textAlign: 'center'
-              }
-            
-              const labelStyles = {
-                paddingRight: 10,
-                color: 'white',
-                fontWeight: 'bold'
-              }
+      const labelStyles = {
+        paddingRight: 10,
+        color: 'white',
+        fontWeight: 'bold'
+      }
 
     return (
-
+      <>
         <div style={containerStyles}>
             <div style={fillerStyles}>
                 <div style={labelStyles}>${sum}</div>
             </div>
         </div>
+        {percent>100 ? <p style={{color:'red', fontWeight:'bold'}}>Warning! Your budget was ${trip.budget} and you have spent ${sum}.</p> : null}
+      </>
     )
 }
 
