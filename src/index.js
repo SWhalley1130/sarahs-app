ab88d2cfdae4950fe9c3c11729926c0463a86488
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-09 16:15:42 -0700

    Dear lord

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './misc-react-starter-code/index.css';
 import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
